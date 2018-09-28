be62f038d582af15c456751427179014eed70699
manyamg <girish.manyam@sky.uk>
2018-09-28 16:39:36 +0100

    callback js examples

--- basics/callbackExamples.js
+++ basics/callbackExamples.js
@@ -1,6 +1,6 @@
 //function passing as parameter
 //a callback is a piece of executable code that is passed as an argument to other code, which is expected to call back (execute) the argument at some convenient time.
-
+//callback is function composition.
 var triple = function(x){
 	return x * 3 ;
 }
