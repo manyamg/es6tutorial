35d8e4d12050d673cc55d42804dae3d2d4e21117
manyamg <girish.manyam@sky.uk>
2018-10-02 15:48:47 +0100

    adding more examples

--- basics/callbackExamples.js
+++ basics/callbackExamples.js
@@ -125,6 +125,30 @@ myArr.sort(function(val,va2){
 
 console.log(myArr);
 
+var friends = ["Mike", "Stacy", "Andy", "Rick"];
+
+friends.forEach(function (eachName, index){
+console.log(index + 1 + ". " + eachName); // 1. Mike, 2. Stacy, 3. Andy, 4. Rick
+});
+
+//callback with params
 
 
+//callback with return of sucess and error
+const isDog = function(animalType, callback){
+    if(animalType === 'Dog'){
+        callback(null, `the result is ${isAnimal}`);
+    } else{
+        callback(new Error("error"));
+    }
+    }
+    
+    isDog('Dog1', function(result, err){
+    if(err){
+        console.log(err);
+    }else
+    {
+        console.log(result);
+    }
+    })
 
