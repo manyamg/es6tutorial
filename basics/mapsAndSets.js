23201f0009bc09a3f44cf2c90f04a02f4b9ee478
manyamg <girish.manyam@sky.uk>
2018-10-09 15:54:48 +0100

    added symbols examples

---
+++ basics/mapsAndSets.js
@@ -0,0 +1,155 @@
+let map = new Map();
+
+map.set('1', 'str1');   // a string key
+map.set(1, 'num1');     // a numeric key
+map.set(true, 'bool1'); // a boolean key
+
+// remember the regular Object? it would convert keys to string
+// Map keeps the type, so these two are different:
+alert( map.get(1)   ); // 'num1'
+alert( map.get('1') ); // 'str1'
+
+alert( map.size ); // 3
+
+let map = new Map([
+    ['1',  'str1'],
+    [1,    'num1'],
+    [true, 'bool1']
+  ]);
+
+
+  let map = new Map(Object.entries({
+    name: "John",
+    age: 30
+  }));  //[ ["name","John"], ["age", 30] ]
+
+  //iteration over map
+
+  let recipeMap = new Map([
+    ['cucumber', 500],
+    ['tomatoes', 350],
+    ['onion',    50]
+  ]);
+  
+  // iterate over keys (vegetables)
+  for (let vegetable of recipeMap.keys()) {
+    alert(vegetable); // cucumber, tomatoes, onion
+  }
+  
+  // iterate over values (amounts)
+  for (let amount of recipeMap.values()) {
+    alert(amount); // 500, 350, 50
+  }
+  
+  // iterate over [key, value] entries
+  for (let entry of recipeMap) { // the same as of recipeMap.entries()
+    alert(entry); // cucumber,500 (and so on)
+  }
+
+  for (const [key, value] of map) {
+    console.log(`${key} = ${value}`); // hobby = cycling  [object Object] = [object Object]
+  }
+  
+  map.forEach((value, key) => {
+    console.log(`${key} = ${value}`);
+  }, map); // hobby = cycling  [object Object] = [object Object]
+  
+
+
+
+//A Set is a collection of values, where each value may occur only once.
+
+let set = new Set();
+
+let john = { name: "John" };
+let pete = { name: "Pete" };
+let mary = { name: "Mary" };
+
+// visits, some users come multiple times
+set.add(john);
+set.add(pete);
+set.add(mary);
+set.add(john);
+set.add(mary);
+
+// set keeps only unique values
+alert( set.size ); // 3
+
+for (let user of set) {
+  alert(user.name); // John (then Pete and Mary)
+}
+
+//Iteration over Set
+let set = new Set(["oranges", "apples", "bananas"]);
+
+for (let value of set) alert(value);
+
+// the same with forEach:
+set.forEach((value, valueAgain, set) => {
+  alert(value);
+});
+
+// The same methods Map has for iterators are also supported:
+
+// set.keys() – returns an iterable object for values,
+// set.values() – same as set.keys, for compatibility with Map,
+// set.entries() – returns an iterable object for entries [value, value], exists for compatibility with Map.
+
+// WeakMaps have several popular use cases. They can be used to keep an object’s private data private,
+let weakMap = new WeakMap();
+
+let obj = {};
+
+weakMap.set(obj, "ok"); // works fine (object key)
+
+weakMap.set("test", "Whoops"); // Error, because "test" is a primitive
+
+let john = { name: "John" };
+
+let weakMap = new WeakMap();
+weakMap.set(john, "...");
+
+john = null; // overwrite the reference
+
+// john is removed from memory!
+
+var key1 = { foo: 1 };
+var key2 = { bar: ['a', 'b', 'c'] };
+
+var x = new WeakMap([[key1, 'value 1'], [key2, 'value 2']]);
+
+console.log(x.get(key1));
+console.log(x.get(key2));  //value 1 value 2
+
+//more examples
+
+var key1 = { foo: 1 };
+var key2 = { bar: ['a', 'b', 'c'] };
+
+var x = new WeakMap([[key1, 'value 1'], [key2, 'value 2']]);
+
+console.log(x.get(key1)); //value 1
+console.log(x.get(key2)); //value 2
+
+x.delete(key1);
+
+console.log(x.get(key1)); //undefined
+console.log(x.get(key2)); //value 2
+
+
+// Use for (... of ...) loop to get the values of an iterable
+for (var x of arr) {
+    console.log(x);
+  } 
+  
+  // Under the covers, for (... of ...) does the following: 
+  var iterator = arr[Symbol.iterator]();
+  var current = iterator.next();
+  while (!current.done) {
+    console.log(current.value);
+    current = iterator.next();
+  }
+
+
+
+
---
+++ basics/sysmbolsExamples.js
@@ -0,0 +1,218 @@
+
+//Symbols are a new primitive type in ES6 - 
+//Symbols are values that programs can create and use as property keys without risking name collisions.
+//They’re immutable once created.
+
+var mySymbol1 = Symbol();
+
+var obj = {};
+var sym = Symbol();
+obj[sym] = "a";
+console.log(obj[sym]);  //a 
+
+
+var obj = {};
+var sym = Symbol();
+obj['hi'] = "bye";
+obj[sym] = "a";
+
+console.log(obj);                    // Prints "{hi: 'bye'}"
+
+for (var i in obj) {
+    console.log(i);                    // Prints "hi"
+}
+
+console.log(JSON.stringify(obj));    // Prints {"hi":"bye"}
+
+
+
+
+// id is a symbol with the description "id" && Symbols are guaranteed to be unique
+let id = Symbol("id");
+
+let id1 = Symbol("id");
+let id2 = Symbol("id");
+
+alert(id1 == id2); // false
+
+
+//In this case the value of symbol is actually taken into account and this way two symbols may be non-unique.
+
+Symbol.for("foo");                // Creates a new global symbol
+var sym = Symbol.for("foo");        // Retrieves the already created symbol
+
+//Symbol.for(key) does return the same symbol each time, so:
+
+Symbol.for("bar") === Symbol.for("bar");    // Evaluates to true
+Symbol("bar") === Symbol("bar");            // Evaluates to false
+
+var sym = Symbol.for("foo");        // Creates new global symbol
+console.log(Symbol.keyFor(sym));    // Prints "foo"
+
+
+//How do I benefit from using Symbol instead of the older data types?
+var persons = {"peter":"pan","jon":"doe"};
+console.log(persons.peter);
+// pan
+
+//What if we have two persons with the name Peter?
+
+var persons = {"peter":"first", "peter":"pan"};
+
+//t's like a person in real life - any person is unique, but their names can be equal. Let's define two "persons".
+var firstPerson = Symbol("peter");
+var secondPerson = Symbol("peter");
+var persons = {[firstPerson]:"first", [secondPerson]:"pan"};
+
+console.log(persons[a]);
+ // first
+ console.log(persons[b]);
+ // pan
+
+ let user = { name: "John" };
+
+// our script uses "id" property
+user.id = "ID Value";
+
+// ...if later another script the uses "id" for its purposes...
+
+user.id = "Their id value"
+// boom! overwritten! it did not mean to harm the colleague, but did it!
+
+let id = Symbol("id");
+let user = {
+  name: "John",
+  age: 30,
+  [id]: 123
+};
+
+
+//Symbols are skipped by for…in
+for (let key in user) alert(key); // name, age (no symbols)
+
+// the direct access by the symbol works
+alert( "Direct: " + user[id] );
+
+let id = Symbol("id");
+let user = {
+  [id]: 123
+};
+
+let clone = Object.assign({}, user);
+
+alert( clone[id] ); // 123
+
+//Symbols real use case - Symbols could be a good replacement for strings or integers as class/module constants:
+
+
+class Application {
+    constructor(mode) {
+      switch (mode) {
+        case Application.DEV:
+          // Set up app for development environment
+          break;
+        case Application.PROD:
+          // Set up app for production environment
+          break;
+         default:
+          throw new Error('Invalid application mode: ' + mode);
+      }
+    }
+  }
+  
+  Application.DEV = Symbol('dev');
+Application.PROD = Symbol('prod');
+
+const app = new Application(Application.DEV);
+
+//Also 
+const directions = {
+    UP   : Symbol( 'UP' ),
+    DOWN : Symbol( 'DOWN' ),
+    LEFT : Symbol( 'LEFT' ),
+    RIGHT: Symbol( 'RIGHT' )
+};
+
+
+
+
+//symbols as keys doesn’t guarantee privacy. 
+const user = {};
+const email = Symbol();
+
+user.name = 'Fred';
+user.age = 30;
+user[email] = 'fred@example.com';
+
+Object.keys(user);
+// <-- Array [ "name", "age" ]
+
+Object.getOwnPropertyNames(user);
+// <-- Array [ "name", "age" ]
+
+JSON.stringify(user);
+// <-- "{"name":"Fred","age":30}"
+
+Object.getOwnPropertySymbols(user);
+// <-- Array [ Symbol() ]
+
+Reflect.ownKeys(user)
+// <-- Array [ "name", "age", Symbol() ]
+
+
+//symbol as an iterator
+
+const band = ['Freddy', 'Brian', 'John', 'Roger'];
+const iterator = band[Symbol.iterator]();
+
+iterator.next().value;
+// <-- { value: "Freddy", done: false }
+iterator.next().value;
+// <-- { value: "Brian", done: false }
+iterator.next().value;
+// <-- { value: "John", done: false }
+iterator.next().value;
+// <-- { value: "Roger", done: false }
+iterator.next().value;
+// <-- { value: undefined, done: true }
+
+
+//more exmaples making object properties private
+let Square = (function() {
+
+    const _width = Symbol('width');
+
+    class Square {
+        constructor( width0 ) {
+            this[_width] = width0;
+        }
+        getWidth() {
+            return this[_width];
+        }
+    }
+
+    return Square;  
+
+} )();
+
+//mor and more examples
+let Person = (() => {
+    let NAME = Symbol('name');
+    return class {
+      constructor(name) {
+        this[NAME] = name;
+      }
+      getName() {
+        return this[NAME];
+      }
+    }
+  })();
+  
+  let person = new Person('Vasya');
+  console.log(person.getName());            // => 'Vasya'
+  console.log(person['name']);              // => undefined
+  console.log(person[Symbol('name')]);      // => undefined, symbols are uniq
+  for (let key in person) console.log(key); // => nothing, symbols are not enumerable
+
+  
+
